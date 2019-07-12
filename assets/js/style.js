21ffd43b688ac2873d21f124a9bab8d19cf700c0
Susep Supriatna <susepsupriatna@gmail.com>
2019-07-12 21:20:20 +0700

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,45 @@
+var about_me = document.querySelector('#about-me');
+about_me.innerHTML = "Hi there, my name is Susep Supriatna, I was born in Subang on May 20, 1999 (year is now 20 years old). <br/><br/> I'am still beginner and i'am still learning about programming.";
+
+function page_about() {
+	window.location.href = "index.html";
+}
+
+function page_educations() {
+	window.location.href = "educations.html";
+}
+
+function page_contact() {
+	window.location.href = "contact.html";
+}
+
+function dicoding() {
+	window.open('https://www.dicoding.com/');
+}
+
+function whatsapp() {
+	window.open('https://wa.me/6285321613063');
+}
+
+function facebook() {
+	window.open('https://www.facebook.com/sspsptn/');
+}
+
+function instagram() {
+	window.open('https://www.instagram.com/sspsptn/');
+}
+
+function github() {
+	window.open('https://github.com/susepsupriatna/');
+}
+
+function linkedin() {
+	window.open('https://www.linkedin.com/in/susep-supriatna-467674150/','_blank');
+}
+
+function email() {
+	window.open('mailto:susepsupriatna@gmail.com','_blank');
+}
+
+
+
